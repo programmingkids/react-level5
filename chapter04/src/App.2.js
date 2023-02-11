9fd3e1ea383a9da4150b12489b2c64295f352ac2
Hiroshi Fujiwara <hiroshi.829f@gmail.com>
2023-02-11 10:03:39 +0000

    added chapter04

---
+++ chapter04/src/App.2.js
@@ -0,0 +1,9 @@
+import { Contact } from './components/Contact';
+
+function App() {
+  return (
+    <Contact />
+  );
+}
+
+export default App;
---
+++ chapter04/src/App.3.js
@@ -0,0 +1,9 @@
+import { Items } from './components/Items';
+
+function App() {
+  return (
+    <Items />
+  );
+}
+
+export default App;
