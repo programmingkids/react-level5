6fdd1ffb26608df4a085a0ad91747dd41d91566b
Hiroshi Fujiwara <hiroshi.829f@gmail.com>
2023-02-11 06:29:37 +0000

    added chapter02

---
+++ chapter02/src/App.1.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="app-container">
+      <h1 className="title">天気予報</h1>
+      <div className="box">
+        <div>晴れのち曇り</div>
+        <div>最高気温：20度</div>
+        <div>最低気温：8度</div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
