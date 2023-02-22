87996e92137604a6222fbfc84bc0da038fa9b070
Hiroshi Fujiwara <hiroshi.829f@gmail.com>
2023-02-22 07:49:21 +0000

    fix chapter05 for main

--- chapter05/src/App.2.js
+++
@@ -1,8 +0,0 @@
-import './App.css';
-import { CountBox } from './components/CountBox';
-
-function App() {
-  return <CountBox />;
-}
-
-export default App;
--- chapter05/src/App.3.js
+++
@@ -1,8 +0,0 @@
-import './App.css';
-import { BookApp } from './components/BookApp';
-
-function App() {
-  return <BookApp />;
-}
-
-export default App;
