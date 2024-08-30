c13b9e11640632f1a23daf1201797bf4802b79cd
Hiroshi Fujiwara <fujiwara@no1s.biz>
2024-08-30 05:34:05 +0000

    delete all contents

--- chapter05/src/components/BookCreateData.js
+++
@@ -1,9 +0,0 @@
-export const createData = ({title, author, price}) => ({
-    title, author, price,
-});
-
-export const createEmptyData = () => createData({
-    title: '',
-    author: '',
-    price: '',
-});
